d2deb8ba1a23b3e8a54f073cff6688076bee78b8
kaushaltheeG <kaushalsk09@gmail.com>
2023-06-11 10:17:45 -0700

    first commit

---
+++ UITesting/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UITesting
+//
+//  Created by Kaushal Kumbagowdana on 6/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITesting/UITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingApp.swift
+//  UITesting
+//
+//  Created by Kaushal Kumbagowdana on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
